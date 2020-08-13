9e217b31b357f649387f059df5e07c6b3e73d750
Andrew <andrewapinon@gmail.com>
2020-08-13 11:58:04 +0800

    changed to aws

--- scripts/blogs/Phuket.js
+++ scripts/blogs/Phuket.js
@@ -14,7 +14,7 @@ const submitComment = async (event) => {
   const commentorName = document.querySelector(".comment-input-name");
   const commentorEmail = document.querySelector(".comment-input-email");
   const comment = document.getElementById("comment-input-form");
-  const endpoint = "ec2-18-219-206-37.us-east-2.compute.amazonaws.com:5000/comments/PhuketPostComment";
+  const endpoint = "http://ec2-18-219-206-37.us-east-2.compute.amazonaws.com:5000/comments/PhuketPostComment";
 
   const bodyObject = {
     name: commentorName.value,
@@ -44,7 +44,7 @@ const submitComment = async (event) => {
 //RENDERING COMMENTS
 
 const renderComments = async () => {
-  const endpoint = "ec2-18-219-206-37.us-east-2.compute.amazonaws.com:5000/getComments/PhuketComments";
+  const endpoint = "http://ec2-18-219-206-37.us-east-2.compute.amazonaws.com:5000/getComments/PhuketComments";
   await fetch(endpoint, {
     method: "GET",
     mode: "cors",
@@ -95,7 +95,7 @@ const getReplies =  (repliesArray,indexOfComment) =>{
       replyId:el
     }
     
-    const endpoint = "ec2-18-219-206-37.us-east-2.compute.amazonaws.com:5000/getComments/PhuketReplies"
+    const endpoint = "http://ec2-18-219-206-37.us-east-2.compute.amazonaws.com:5000/getComments/PhuketReplies"
     await fetch(endpoint,{
     mode: "cors",
     method: "POST",
@@ -201,7 +201,7 @@ const submitReply = async (event,commentId) =>{
   const email = document.getElementById("reply-input-email");
   const comment = document.getElementById("reply-input-comment");
   
-  const endpoint = "ec2-18-219-206-37.us-east-2.compute.amazonaws.com:5000/comments/PhuketPostReply"
+  const endpoint = "http://ec2-18-219-206-37.us-east-2.compute.amazonaws.com:5000/comments/PhuketPostReply"
   
   const replyObj = {
     name: name.value,
