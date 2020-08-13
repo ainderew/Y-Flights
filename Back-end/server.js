6258f585700794366aa354cc2c121ff5ce50367a
Andrew <andrewapinon@gmail.com>
2020-08-13 12:58:51 +0800

    alkdjf

--- Back-end/server.js
+++ Back-end/server.js
@@ -3,20 +3,29 @@ const app = express();
 const mongoose = require("mongoose");
 const cors = require("cors");
 const bodyParser = require('body-parser')
+const fs = require("fs")
+const https = require("https")
+var options = {
+  key: fs.readFileSync("./server-key.pem"),
+  cert: fs.readFileSync('./server-cert.pem'),
+};
 
 require("dotenv").config();
 
 // START =============================================================================================================
-const PORT = process.env.PORT || 443;
+const PORT = process.env.PORT || 5000;
 
 app.use(cors());
 app.use(bodyParser.urlencoded({ extended: true }))
 app.use(bodyParser.json())
 
-app.listen(PORT, () => {
-  console.log(`using port: ${PORT}`);
-});
+// app.listen(PORT, () => {
+//   console.log(`using port: ${PORT}`);
+// });
 
+const server = https.createServer(options, app).listen(PORT, function(){
+  console.log("Express server listening on port " + PORT);
+});
 //CONNECT TO MONGODB ATLAS
 mongoose.connect(`mongodb+srv://andrewapinon:${process.env.MONGODB_PASSWORD}@yflights.hs835.mongodb.net/YFlight`, { useNewUrlParser: true, useUnifiedTopology: true }, () =>{
   console.log("Connected to DB")
