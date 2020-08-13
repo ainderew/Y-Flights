7f93aaa48cdb4343b2b0a74adfe02423e1cc82f9
Andrew <andrewapinon@gmail.com>
2020-08-13 12:34:18 +0800

    changed to aws

--- Back-end/server.js
+++ Back-end/server.js
@@ -7,7 +7,7 @@ const bodyParser = require('body-parser')
 require("dotenv").config();
 
 // START =============================================================================================================
-const PORT = process.env.PORT || 443;
+const PORT = process.env.PORT || 5000;
 
 app.use(cors());
 app.use(bodyParser.urlencoded({ extended: true }))
