3efa8aa5d54e6106943006226372e1caff15ca7b
Andrew <andrewapinon@gmail.com>
2020-10-07 16:52:29 +0800

    API testing

--- scripts/travelPayoutAPI.js
+++ scripts/travelPayoutAPI.js
@@ -4,7 +4,7 @@ const token = "bc3e586d4ac8295cde912de926fa9a5e";
 
 const cheapTickets = async () => {
   await fetch(
-    `http://api.travelpayouts.com/v1/prices/cheap?origin=MOW&destination=HKT&depart_date=&return_date=&token=${token}`,
+    `https://api.travelpayouts.com/v1/prices/cheap?origin=MOW&destination=HKT&depart_date=&return_date=&token=${token}`,
     {
       method: "GET",
       mode: "cors",
