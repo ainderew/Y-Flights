533716380674e825017c9a040e1ea3ff71e23bda
Andrew <andrewapinon@gmail.com>
2020-10-09 13:36:40 +0800

    api inegration

--- Back-end/Routes/TravelPayouts.js
+++ Back-end/Routes/TravelPayouts.js
@@ -1,11 +1,21 @@
-const express = require("express")
+const express = require("express");
 const router = express.Router();
-const fetch = require('node-fetch');
+const fetch = require("node-fetch");
 
+router.get("/CheapTickets", async (req, res) => {
+  
 
-router.get("/CheapTickets", async (req,res) =>{
-    await fetch(
-        `https://api.travelpayouts.com/v1/prices/cheap?origin=MOW&destination=HKT&depart_date=&return_date=&token=${process.env.TRAVEL_PAYOUTS_API_TOKEN}`,
+  await fetch(`http://www.travelpayouts.com/whereami?locale=en&callback=&ip=`, {
+    method: "GET",
+    mode: "cors",
+    headers: {
+      "Content-Type": "Application/json",
+    },
+  })
+    .then(response => response.json())
+    .then(data => {
+       fetch(
+        `https://api.travelpayouts.com/v1/prices/cheap?origin=${data.iata}&destination=SGP&depart_date=&return_date=&token=${process.env.TRAVEL_PAYOUTS_API_TOKEN}`,
         {
           method: "GET",
           mode: "cors",
@@ -16,7 +26,15 @@ router.get("/CheapTickets", async (req,res) =>{
       )
         .then(response => response.json())
         .then(data => res.json(data));
+      
+    });
     
-})
+    
+    
 
-module.exports = router
+  
+});
+
+
+
+module.exports = router;
